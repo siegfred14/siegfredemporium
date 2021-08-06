c268e3d066f9f8c206c01e2e70b9e5709cea1620
Siegfred Samson <siegfredsamson@gmail.com>
2021-08-07 00:49:59 +0100

    Register View created

--- authentication/views.py
+++ authentication/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from rest_framework import GenericAPIView
 
 # Create your views here.
+class RegisterView(GenericAPIView):
+    def post(self, request):
+        pass
