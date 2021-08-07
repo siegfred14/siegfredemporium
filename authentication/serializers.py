6e8b7b7db7717128f6e505bf41f0869a3b215e50
Siegfred Samson <siegfredsamson@gmail.com>
2021-08-08 00:27:55 +0100

    UserSerializers added

--- authentication/serializers.py
+++ authentication/serializers.py
@@ -8,3 +8,7 @@ class UserSerializer(serializers.ModelSerializer):
     email = serializers.EmailField(max_length=255, min_length=4)
     first_name = serializers.CharField(max_length=255, min_length=2)
     last_name = serializers.CharField(max_length=255, min_length=2)
+
+    class Meta:
+        model = User
+        fields = ['username', 'first_name', 'last_name', 'email']
