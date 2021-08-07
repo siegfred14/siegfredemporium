b6390a31b04365495fd0ecf34ab7c7c77386faeb
Siegfred Samson <siegfredsamson@gmail.com>
2021-08-08 00:21:42 +0100

    def post added to RegisterView

--- authentication/views.py
+++ authentication/views.py
@@ -1,7 +1,14 @@
 from django.shortcuts import render
-from rest_framework import GenericAPIView
+from rest_framework import GenericAPIView, serializers
+from .serializers import UserSerializer
 
 # Create your views here.
+
+
 class RegisterView(GenericAPIView):
+    serializer_class = UserSerializer
+
     def post(self, request):
+        serializer = UserSerializer(data=request.data)
+
         pass
