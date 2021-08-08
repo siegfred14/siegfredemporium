d46a6e9bea8a2c8ee81c3f228046cce4156c5e90
Siegfred Samson <siegfredsamson@gmail.com>
2021-08-08 12:35:33 +0100

    import statement modified

--- authentication/views.py
+++ authentication/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from rest_framework import GenericAPIView, serializers
+from rest_framework.generics import GenericAPIView
 from .serializers import UserSerializer
 from rest_framework.response import Response
 from rest_framework import status
