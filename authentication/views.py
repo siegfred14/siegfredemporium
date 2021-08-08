228d4018af568f4a9c7804726b05f06f4ef73a12
Siegfred Samson <siegfredsamson@gmail.com>
2021-08-08 10:51:41 +0100

    status responses added

--- authentication/views.py
+++ authentication/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from rest_framework import GenericAPIView, serializers
 from .serializers import UserSerializer
+from rest_framework.response import Response
+from rest_framework import status
 
 # Create your views here.
 
@@ -13,3 +15,6 @@ class RegisterView(GenericAPIView):
 
         if serializer.is_valid():
             serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
