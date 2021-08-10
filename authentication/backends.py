027d7c09df668fdb6a14c187566e4f79bec5444e
Siegfred Samson <siegfredsamson@gmail.com>
2021-08-10 12:12:21 +0100

    backends.py updated to authenticate secret key

--- authentication/backends.py
+++ authentication/backends.py
@@ -1,8 +1,20 @@
 import jwt
 from rest_framework import authentication
-
+from django.conf import settings
 
 class JWTAuthentication(authentication.BaseAuthentication):
 
     def authenticate(self, request):
+        auth_data=authentication.get_authorization_header(request)
+
+        if not auth_data:
+            return None
+
+        prefix, token = auth_data.decode('utf-8').split(' ')
+
+        try:
+            payload=jwt.decode(token, )
+        except expression as identifier:
+            pass
+
         return super().authenticate(request)
