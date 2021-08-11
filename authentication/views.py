c09f4d7f54bc3415e9d045218f106d1805313827
Siegfred Samson <siegfredsamson@gmail.com>
2021-08-12 00:05:13 +0100

    settings imported to authentication/views.py

--- authentication/views.py
+++ authentication/views.py
@@ -3,6 +3,7 @@ from rest_framework.generics import GenericAPIView
 from .serializers import UserSerializer
 from rest_framework.response import Response
 from rest_framework import status
+from django.conf import settings
 
 # Create your views here.
 
