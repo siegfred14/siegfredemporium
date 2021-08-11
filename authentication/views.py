2822ba990a5bef52c04cf2c1bd87f2ef24d80401
Siegfred Samson <siegfredsamson@gmail.com>
2021-08-12 00:07:05 +0100

    auth imported to authentication/views.py

--- authentication/views.py
+++ authentication/views.py
@@ -4,6 +4,7 @@ from .serializers import UserSerializer
 from rest_framework.response import Response
 from rest_framework import status
 from django.conf import settings
+from django.contrib import auth
 
 # Create your views here.
 
