4c3bbdf93257848f1a8452932b76acf06036978c
Siegfred Samson <siegfredsamson@gmail.com>
2021-08-13 00:37:58 +0100

    login registered in urls.py

--- authentication/urls.py
+++ authentication/urls.py
@@ -1,8 +1,10 @@
+from re import L
 from django.urls import path
-from django.urls.resolvers import URLPattern
-from .views import RegisterView
+from django.urls import path
+from .views import RegisterView, LoginView
 
 
 urlpatterns = [
-    path('register', RegisterView.as_view())
+    path('register', RegisterView.as_view()),
+    path('login', LoginView.as_view())
 ]
